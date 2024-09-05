abd15decb5760cce7e0e39aeee1b32e6eb74c95b
Arvinder Singh <arvinder.singh@esure.com>
2024-09-05 16:55:39 +0100

    update

--- src/test-utils/vitest.setup.tsx
+++ src/test-utils/vitest.setup.tsx
@@ -5,9 +5,6 @@ import { ReactElement } from 'react';
 import { render, RenderOptions } from '@testing-library/react';
 import { LocaleProvider } from '../contexts/LocaleProvider';
 
-const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) =>
-  render(ui, { wrapper: LocaleProvider, ...options });
-
 beforeAll(() => {
   rootStore.resetForm();
   server.listen({ onUnhandledRequest: 'error' });
@@ -20,6 +17,9 @@ afterEach(() => {
 afterAll(() => {
   server.close();
 });
+
+const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) =>
+  render(ui, { wrapper: LocaleProvider, ...options });
 
 // eslint-disable-next-line react-refresh/only-export-components
 export * from '@testing-library/react';
